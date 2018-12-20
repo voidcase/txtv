b24060e3994d936944b474eda89480f0c0828e40
Isak Lindhé <isak.e.lindhe@gmail.com>
2018-12-20 21:43:22 +0100

    yellow color segments

--- txtv.py
+++ txtv.py
@@ -1,13 +1,22 @@
 import bs4
 import requests as rq
+import colorama
+from colorama import Fore, Back, Style
 from sys import argv
 
 
 if __name__ == '__main__':
+    colorama.init()
     page = int(argv[1])
     res = rq.get(f'https://www.svt.se/svttext/web/pages/{page}.html')
     soup = bs4.BeautifulSoup(res.content, 'html.parser')
     root = soup.find('pre', class_='root')
-    rows = [ s.get_text() for s in root.find_all() if s.get_text() != ' ' ]
-    for r in rows:
-        print(r)
+    rows = root.find_all('span')
+    for row in rows:
+        if row.get_text() == ' ' or 'bgB' in row.attrs['class']:
+            continue
+        if 'Y' in row.attrs['class']:
+            print(Fore.YELLOW + row.get_text() + Fore.RESET)
+        else:
+            print(row.get_text())
+    colorama.deinit()
